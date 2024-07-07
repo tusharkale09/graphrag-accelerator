56c1cb11df82ac611d3198373e6419ea053c832b
Tushar Kale <152104420+tusharkale09@users.noreply.github.com>
2024-07-07 20:50:41 +0200

    QS_update

--- notebooks/tests/test_magic.py
+++ notebooks/tests/test_magic.py
@@ -4,4 +4,4 @@ file_magic = magic.Magic()
 file_magic = magic.Magic(magic_file=r'C:\\GnuWin32\\share\\misc\\magic.mgc c:\users\tkae6m\anaconda3\envs\py38_env\lib\site-packages')  # Adjust the path as needed
 
 # Test file type detection
-# print(file_magic.from_file('example.txt'))  # Replace with a valid file path
+print(file_magic.from_file(r'C:\\GnuWin32\\share\\misc\\magic.mgc c:\users\tkae6m\anaconda3\envs\py38_env\lib\site-packages'))  # Replace with a valid file path
