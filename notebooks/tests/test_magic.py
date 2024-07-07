80a37ad9bcd04bb2c0837ebb17c0f8b7b3218f92
Tushar Kale <152104420+tusharkale09@users.noreply.github.com>
2024-07-07 20:24:31 +0200

    Updated test_magic script

--- notebooks/tests/test_magic.py
+++ notebooks/tests/test_magic.py
@@ -1,5 +1,7 @@
-import magic
-
 file_magic = magic.Magic()
 
-print(file_magic.from_file('tusharkale09/graphrag-accelerator/notebooks/1-Quickstart.ipynb'))
+# Specify the path to the magic file
+file_magic = magic.Magic(magic_file=r'C:\\GnuWin32\\share\\misc\\magic.mgc c:\users\tkae6m\anaconda3\envs\py38_env\lib\site-packages')  # Adjust the path as needed
+
+# Test file type detection
+# print(file_magic.from_file('example.txt'))  # Replace with a valid file path
